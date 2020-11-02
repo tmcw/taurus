050aa1c5eff6ed060e75379ef4b0dac6bb9ea1f9
Tom MacWright <tom@macwright.org>
2020-11-01 16:12:34 -0800

    Initial Commit

---
+++ taurus/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  taurus
+//
+//  Created by Tom MacWright on 11/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
