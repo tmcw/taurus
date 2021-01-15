c476b58024171a643d2fd820978194ff1527f31f
Tom MacWright <tom@macwright.org>
2021-01-15 09:25:13 -0800

    Type system first draft

--- taurus/GeminiCompiler.swift
+++ taurus/GeminiCompiler.swift
@@ -9,87 +9,72 @@ import Foundation
 
 struct Position: Equatable {
     var start: Pos;
-    var end: Pos? = nil;
+    var end: Pos;
+    init(start: Pos, end: Pos) {
+        self.start = start
+        self.end = end
+    }
+    init(fromToken t: Token) {
+        start = t.start
+        end = t.end
+    }
+}
+
+enum Data: Equatable {
+    case root
+    case list
+    case brk
+    case listItem(value: String)
+    case text(value: String)
+    case heading(value: String, rank: Int)
+    case quote(value: String)
+    case pre(value: String, alt: String?)
+    case link(value: String, url: String)
 }
 
 struct Node: Equatable {
-    var type: String;
-    var value: String? = nil;
+    var data: Data;
+    var position: Position
     var children: [Node] = [];
-    var position: Position;
-    var alt: String? = nil;
-    var url: String? = nil;
-    var rank: Int? = nil;
 }
 
 func compileGemini(tokens: [Token]) -> Node {
-    func point(d: Pos) -> Pos {
-        // TODO: just clone?
-        return Pos(line: d.line, column: d.column, offset: d.offset)
-    }
+    let rootPosition = Position(
+        start: tokens[0].start,
+        end: tokens[tokens.count - 1].end
+    )
+    var rootNode: Node = Node(
+        data: Data.root,
+        position: rootPosition,
+        children: []
+    );
     var stack: [Node] = [
-        Node(
-            type: "root",
-            children: [],
-            position: Position(
-                start: point(d: tokens[0].start),
-                end: point(d: tokens[tokens.count - 1].end)
-            )
-        )
-    ]
+      rootNode
+    ];
     var index = 0
     var token: Token
     var node: Node
     var values: [String]
     
-    func enter(type: String, value: String? = nil, rank: Int? = nil, alt: String? = nil, url: String? = nil, children: [Node] = [], token: Token) -> Node {
-        var node = Node(
-            type: type,
-            value: value,
-            children: children,
-            position: Position(start: point(d: token.start)),
-            alt: alt,
-            url: url,
-            rank: rank
-        )
-        stack[stack.endIndex - 1].children.append(node)
-        stack.append(node)
-        return node
-    }
-    
-    func exit(token: Token) -> Node {
-        var node = stack.removeLast()
-        node.position.end = point(d: token.end)
-        return node
+    func addNode(node: Node) {
+        stack[stack.count - 1].children.append(node);
     }
 
     while (index < tokens.count - 1) {
         token = tokens[index]
         
         if (token.type == "eol" && token.hard != nil) {
-            _ = enter(type: "break", token: token)
-            _ = exit(token: token)
+            addNode(node: Node(data: Data.brk, position: Position(fromToken: token)))
         } else if (token.type == "headingSequence") {
-            
             if (tokens[index + 1].type == "whitespace") {
                 index += 1
             }
             if (tokens[index + 1].type == "headingText") {
                 index += 1
-                node = enter(
-                    type: "heading",
-                    value: tokens[index].value,
-                    rank: token.value.count,
-                    token: token
-                )
+                addNode(node: Node(data: Data.heading(value: tokens[index].value, rank: token.value.count), position: Position(fromToken: token)))
             } else {
-                node = enter(
-                    type: "heading", value: "", rank: token.value.count,
-                    token: token
-                )
+                addNode(node: Node(data: Data.heading(value: "", rank: token.value.count), position: Position(fromToken: token)))
             }
-            
-            _ = exit(token: tokens[index])
         } else if (token.type == "linkSequence") {
             var value = ""
             var url = ""
@@ -110,26 +95,21 @@ func compileGemini(tokens: [Token]) -> Node {
                 }
             }
             
-            node = enter(type: "link", value: value, url: url, token: token)
-            
-            _ = exit(token: tokens[index])
-        } else if (token.type == "listSequence") {
-            if (stack[stack.count - 1].type != "list") {
-                _ = enter(type: "list", children: [], token: token)
+            addNode(node: Node(data: Data.link(value: value, url: url), position: Position(fromToken: token)))
+        } /* else if (token.type == "listSequence") {
+            if (stack[stack.count - 1] is ListNode) {
+                enterNode(node: ListNode(position: Position(start: token.start))
             }
-            
             
             if (tokens[index + 1].type == "whitespace") {
                 index += 1
             }
             if (tokens[index + 1].type == "listText") {
                 index += 1
-                node = enter(type: "listItem", value: tokens[index].value, token: token)
+                node = enterNode(node: ListItemNode(value: tokens[index].value, position: Position(fromToken: token)))
             } else {
-                node = enter(type: "listItem", value: "", token: token)
+                node = enterNode(node: ListItemNode(value: "", position: Position(fromToken: token.start)))
             }
-            
-            _ = exit(token: tokens[index])
             
             if (
                 tokens[index + 1].type != "eol" ||
@@ -137,7 +117,7 @@ func compileGemini(tokens: [Token]) -> Node {
             ) {
                 _ = exit(token: tokens[index])
             }
-        } else if (token.type == "preSequence") {
+        } */ else if (token.type == "preSequence") {
             values = []
             var alt: String? = nil
             
@@ -171,29 +151,27 @@ func compileGemini(tokens: [Token]) -> Node {
                 index += 1
             }
             
-            node = enter(type: "pre", value: values.joined(separator: ""), alt: alt, token: token)
             
-            _ = exit(token: tokens[index])
+            addNode(node: Node(data: Data.pre(value: values.joined(separator: ""),
+                                           alt: alt),
+                                           position: Position(fromToken: token)))
+            
         } else if (token.type == "quoteSequence") {
-            
             if (tokens[index + 1].type == "whitespace") {
                 index += 1
             }
             if (tokens[index + 1].type == "quoteText") {
                 index += 1
-                node = enter(type: "quote", value: tokens[index].value, token: token)
+                addNode(node: Node(data: Data.quote(value: tokens[index].value), position: Position(fromToken: token)))
             } else {
-                node = enter(type: "quote", value: "", token: token)
+                addNode(node: Node(data: Data.quote(value: ""), position: Position(fromToken: token)))
             }
-            
-            _ = exit(token: tokens[index])
         } else if (token.type == "text") {
-            _ = enter(type: "text", value: token.value, token: token)
-            _ = exit(token: token)
+            addNode(node: Node(data: Data.text(value: token.value), position: Position(fromToken: token)))
         }
         // Else would be only soft EOLs and EOF.
         index += 1
     }
     
-    return stack[0]
+    return stack.first!;
 }
--- taurusTests/GeminiCompilerTests.swift
+++ taurusTests/GeminiCompilerTests.swift
@@ -19,87 +19,97 @@ class GeminiCompilerTests: XCTestCase {
 
     func testParseHeader() throws {
         XCTAssertEqual(compileGemini(tokens: parseGemini()("## Test", true)),
-                       Node(type: "root", value: nil, children: [taurus.Node(type: "heading", value: Optional("Test"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: Optional(2))], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 8, offset: 7))), alt: nil, url: nil, rank: nil))
+                       taurus.Node(
+                           data: taurus.Data.root,
+                           position: taurus.Position(
+                               start: taurus.Pos(line: 1, column: 1, offset: 0),
+                               end: taurus.Pos(line: 1, column: 8, offset: 7)
+                           ),
+                           children: [
+                               taurus.Node(data: taurus.Data.heading(value: "Test", rank: 2),
+                                           position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: taurus.Pos(line: 1, column: 3, offset: 2)), children: []),
+                           ]
+                       ))
     }
 
-    func testParseList() throws {
-        XCTAssertEqual(compileGemini(tokens: parseGemini()("* Yes", true)),
-                       Node(type: "root", value: nil, children: [taurus.Node(type: "list", value: nil, children: [],
-                                                                             position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil)],
-                       position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0),
-                                                 end: Optional(taurus.Pos(line: 1, column: 6, offset: 5))), alt: nil, url: nil, rank: nil))
-    }
-    
-    func testParseLink() throws {
-        XCTAssertEqual(compileGemini(tokens: parseGemini()("=> https://foo.com/", true)),
-                       Node(type: "root", value: nil, children: [taurus.Node(type: "link", value: Optional(""), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: Optional("https://foo.com/"), rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 20, offset: 19))), alt: nil, url: nil, rank: nil))
-    }
-    
-    func testParseLinkWithDescription() throws {
-        XCTAssertEqual(compileGemini(tokens: parseGemini()("=> https://foo.com/ Foo", true)),
-                       Node(type: "root", value: nil, children: [taurus.Node(type: "link", value: Optional("Foo"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: Optional("https://foo.com/"), rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 24, offset: 23))), alt: nil, url: nil, rank: nil))
-    }
-    
-    func testPre() throws {
-        XCTAssertEqual(compileGemini(tokens: parseGemini()(
-                                        """
-```js
-test
-```
-""", true)),
-        Node(type: "root", value: nil, children: [
-                taurus.Node(type: "pre", value: Optional("\ntest"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: Optional("js"), url: nil, rank: nil)
-        ], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 3, column: 4, offset: 14))), alt: nil, url: nil, rank: nil))
-    }
-    
-    
-    func testText() throws {
-        XCTAssertEqual(compileGemini(tokens: parseGemini()("yes", true)),
-                       Node(type: "root", value: nil, children: [taurus.Node(type: "text", value: Optional("yes"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 4, offset: 3))), alt: nil, url: nil, rank: nil))
-    }
-    
-    
-    func testQuote() throws {
-        XCTAssertEqual(compileGemini(tokens: parseGemini()("> yes", true)),
-                      Node(type: "root", value: nil, children: [taurus.Node(type: "quote", value: Optional("yes"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 6, offset: 5))), alt: nil, url: nil, rank: nil))
-    }
-    
-    func testEmptyQuote() throws {
-        XCTAssertEqual(compileGemini(tokens: parseGemini()(">", true)),
-                      Node(type: "root", value: nil, children: [taurus.Node(type: "quote", value: Optional(""), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 2, offset: 1))), alt: nil, url: nil, rank: nil))
-    }
-    
-    func testEmptyList() throws {
-        XCTAssertEqual(compileGemini(tokens: parseGemini()("*", true)),
-                     Node(type: "root", value: nil, children: [taurus.Node(type: "list", value: nil, children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 2, offset: 1))), alt: nil, url: nil, rank: nil))
-    }
-    
-    
-    func testBreak() throws {
-        XCTAssertEqual(compileGemini(tokens: parseGemini()("""
-Yes
+    /*
 
-Hello
-""", true)),
-                       
-                    Node(type: "root", value: nil, children: [
-                            taurus.Node(type: "text", value: Optional("Yes"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil),
-                            taurus.Node(type: "text", value: Optional("Hello"), children: [], position: taurus.Position(start: taurus.Pos(line: 3, column: 1, offset: 5), end: nil), alt: nil, url: nil, rank: nil)
-                    ], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 3, column: 6, offset: 10))), alt: nil, url: nil, rank: nil))
-    }
-    
-    func testEmptyHeader() throws {
-        XCTAssertEqual(compileGemini(tokens: parseGemini()("#", true)),
-                    Node(type: "root", value: nil, children: [taurus.Node(type: "heading", value: Optional(""), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: Optional(1))], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 2, offset: 1))), alt: nil, url: nil, rank: nil))
-    }
-    
-    func testIgnoredClosingAlt() throws {
-        XCTAssertEqual(compileGemini(tokens: parseGemini()("""
-```js
-yeah
-```js
-""", true)),
-                       
-            Node(type: "root", value: nil, children: [taurus.Node(type: "pre", value: Optional("\nyeah"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: Optional("js"), url: nil, rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 3, column: 6, offset: 16))), alt: nil, url: nil, rank: nil))
-    }
+         func testParseList() throws {
+             XCTAssertEqual(compileGemini(tokens: parseGemini()("* Yes", true)),
+                            Node(type: "root", value: nil, children: [taurus.Node(type: "list", value: nil, children: [],
+                                                                                  position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil)],
+                            position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0),
+                                                      end: Optional(taurus.Pos(line: 1, column: 6, offset: 5))), alt: nil, url: nil, rank: nil))
+         }
+
+         func testParseLink() throws {
+             XCTAssertEqual(compileGemini(tokens: parseGemini()("=> https://foo.com/", true)),
+                            Node(type: "root", value: nil, children: [taurus.Node(type: "link", value: Optional(""), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: Optional("https://foo.com/"), rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 20, offset: 19))), alt: nil, url: nil, rank: nil))
+         }
+
+         func testParseLinkWithDescription() throws {
+             XCTAssertEqual(compileGemini(tokens: parseGemini()("=> https://foo.com/ Foo", true)),
+                            Node(type: "root", value: nil, children: [taurus.Node(type: "link", value: Optional("Foo"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: Optional("https://foo.com/"), rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 24, offset: 23))), alt: nil, url: nil, rank: nil))
+         }
+
+         func testPre() throws {
+             XCTAssertEqual(compileGemini(tokens: parseGemini()(
+                                             """
+     ```js
+     test
+     ```
+     """, true)),
+             Node(type: "root", value: nil, children: [
+                     taurus.Node(type: "pre", value: Optional("\ntest"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: Optional("js"), url: nil, rank: nil)
+             ], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 3, column: 4, offset: 14))), alt: nil, url: nil, rank: nil))
+         }
+
+         func testText() throws {
+             XCTAssertEqual(compileGemini(tokens: parseGemini()("yes", true)),
+                            Node(type: "root", value: nil, children: [taurus.Node(type: "text", value: Optional("yes"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 4, offset: 3))), alt: nil, url: nil, rank: nil))
+         }
+
+         func testQuote() throws {
+             XCTAssertEqual(compileGemini(tokens: parseGemini()("> yes", true)),
+                           Node(type: "root", value: nil, children: [taurus.Node(type: "quote", value: Optional("yes"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 6, offset: 5))), alt: nil, url: nil, rank: nil))
+         }
+
+         func testEmptyQuote() throws {
+             XCTAssertEqual(compileGemini(tokens: parseGemini()(">", true)),
+                           Node(type: "root", value: nil, children: [taurus.Node(type: "quote", value: Optional(""), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 2, offset: 1))), alt: nil, url: nil, rank: nil))
+         }
+
+         func testEmptyList() throws {
+             XCTAssertEqual(compileGemini(tokens: parseGemini()("*", true)),
+                          Node(type: "root", value: nil, children: [taurus.Node(type: "list", value: nil, children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 2, offset: 1))), alt: nil, url: nil, rank: nil))
+         }
+
+         func testBreak() throws {
+             XCTAssertEqual(compileGemini(tokens: parseGemini()("""
+     Yes
+
+     Hello
+     """, true)),
+
+                         Node(type: "root", value: nil, children: [
+                                 taurus.Node(type: "text", value: Optional("Yes"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: nil),
+                                 taurus.Node(type: "text", value: Optional("Hello"), children: [], position: taurus.Position(start: taurus.Pos(line: 3, column: 1, offset: 5), end: nil), alt: nil, url: nil, rank: nil)
+                         ], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 3, column: 6, offset: 10))), alt: nil, url: nil, rank: nil))
+         }
+
+         func testEmptyHeader() throws {
+             XCTAssertEqual(compileGemini(tokens: parseGemini()("#", true)),
+                         Node(type: "root", value: nil, children: [taurus.Node(type: "heading", value: Optional(""), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: nil, url: nil, rank: Optional(1))], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 1, column: 2, offset: 1))), alt: nil, url: nil, rank: nil))
+         }
+
+         func testIgnoredClosingAlt() throws {
+             XCTAssertEqual(compileGemini(tokens: parseGemini()("""
+     ```js
+     yeah
+     ```js
+     """, true)),
+
+                 Node(type: "root", value: nil, children: [taurus.Node(type: "pre", value: Optional("\nyeah"), children: [], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: nil), alt: Optional("js"), url: nil, rank: nil)], position: taurus.Position(start: taurus.Pos(line: 1, column: 1, offset: 0), end: Optional(taurus.Pos(line: 3, column: 6, offset: 16))), alt: nil, url: nil, rank: nil))
+         }
+         */
 }
